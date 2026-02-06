291ef3dbaec9f41cefcdfe07eca82a6f346630c8
Praveen Naradwar <naradwarpraveen1997@gmail.com>
2026-02-06 14:28:33 +0530

    testingjob-21

--- read_data_api.py
+++ read_data_api.py
@@ -6,7 +6,7 @@ url="https://jsonplaceholder.typicode.com/users"
 
 response=requests.get(url)
 if response.status_code==200:
-    data=reponse.json()
+    data=response.json()
 
 df=pd.DataFrame(data)
 print(df)
